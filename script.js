d1cdc7a594de707089dfbc1bef40428f243acccf
ÖMER KISAKOL <okisakol@gmail.com>
2024-01-22 00:16:45 +0300

    Anasayfa

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ document.getElementById('dropdownMenuButton2').addEventListener('mouseleave', fu
     this.setAttribute('aria-expanded', 'false');
     this.nextElementSibling.classList.remove('show');
 });
+
+const nextIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">\n' +
+    '  <path style="color: #ff6720;" fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>\n' +
+    '</svg>';
+const prevIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">\n' +
+    '  <path style="color: #ff6720;" fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>\n' +
+    '</svg>';
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:5,
+        margin:10,
+        nav:true,
+        mouseDrag:false,
+        center: true,
+        navText: [
+            prevIcon,
+            nextIcon
+        ],
+        responsive:{
+            0:{
+                items:1
+            },
+            560:{
+                items:1.5
+            },
+            740:{
+                items:2
+            },
+            991:{
+                items:2.5
+            },
+            1150:{
+                items:3
+            },
+
+        }
+    });});
